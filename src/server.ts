30518482eb6fd29de7fc842520ce92a674190516
Mateus Nadal <nadalmateus@outlook.com>
2021-04-20 13:52:23 -0300

    feat(settings): settings entities and route

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,11 @@
 import express from "express";
+
 import "./database";
+import { routes } from "./routes";
 
 const app = express();
-
 app.use(express.json());
 
-app.get("/", (request, response) => {
-  return response.json({ message: "Hello World" });
-});
-
-app.post("/", (request, response) => {
-  return response.json({ message: "Usuario Salvo" });
-});
+app.use(routes);
 
 app.listen(3333, () => console.log("Server running on port 3333"));
