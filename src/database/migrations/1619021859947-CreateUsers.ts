0626f596ae9255c5ba72dc562468536481a750ba
Mateus Nadal <nadalmateus@outlook.com>
2021-04-21 14:46:38 -0300

    feat(users): user and Settigns entities

--- src/database/migrations/1619021859947-CreateUsers.ts
+++ src/database/migrations/1619021859947-CreateUsers.ts
@@ -1,10 +1,10 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm'
 
-export class CreateSettings1618926807820 implements MigrationInterface {
+export class CreateUsers1619021859947 implements MigrationInterface {
   public async up (queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
-        name: 'settings',
+        name: 'users',
         columns: [
           {
             name: 'id',
@@ -12,30 +12,21 @@ export class CreateSettings1618926807820 implements MigrationInterface {
             isPrimary: true
           },
           {
-            name: 'username',
+            name: 'email',
             type: 'varchar'
-          },
-          {
-            name: 'chat',
-            type: 'boolean',
-            default: true
-          },
-          {
-            name: 'updated_at',
-            type: 'timestamp',
-            default: 'now()'
           },
           {
             name: 'created_at',
             type: 'timestamp',
             default: 'now()'
           }
+
         ]
       })
     )
   }
 
   public async down (queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropTable('settings')
+    await queryRunner.dropTable('users')
   }
 }
